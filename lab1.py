b667aebdc12f3a29688896b24fdbc44b2882af50
Chaitali Patel <cpatel1@cs.ualberta.ca>
2019-01-09 18:10:33 -0700

    Add gitignore, requirements, script

--- lab1.py
+++ lab1.py
@@ -1,6 +1,13 @@
+#!/usr/bin/env python
+
 import requests
 print(requests.__version__)
 
 r = requests.get("http://www.google.com")
-print(r.status_code)
-print(r.text)
+#print(r.status_code)
+#print(r.text)
+#print(dir(r))
+
+a = requests.get("https://raw.githubusercontent.com/chaitali/cmput404/master/lab1.py")
+print(a.text)
+
